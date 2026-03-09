110784869387c8f9cae5805a398c262833931cb4
TheYautja <am.96138669@gmail.com>
2026-03-08 22:37:03 -0300

    add function to check display state

--- lib/device.dart
+++ lib/device.dart
@@ -4,16 +4,31 @@ import 'dart:io';
 
 class Device {
 	
+	String rom_path = "lib/chiplogo.ch8";
+	Uint8List memory = Uint8List(4096);
+	Uint16List PC = Uint16List(1);
+	Uint16List I = Uint16List(1);
+	Uint16List stack = Uint16List(16);
+	//sound/display timers (60hz/s)
+	Uint8List registers = Uint8List(16);
+	List<List<bool>> display = List.generate(32, (_) => List.filled(64, false));
 	
-	var memory = Uint8List(4096);
-	int PC = 0;
-	var I = Uint16List(1);
-	//add 16 bit stack and sound/display timers (60hz/s)
-	var registers = Uint8List(16);
+	
+	bool print_display_state(){
+		String line = "";
+		for(int i = 0; i < 32; i++){
+			for(int j = 0; j < 64; j++){
+				line += display[i][j].toString() + " ";
+			}
+			print(line);
+			line = "";
+		}
+		return true;
+	}
 	
 	
 	Future<bool> load_rom_into_memory() async {
-		var rom = File("lib/AstroDodge.ch8");
+		var rom = File(rom_path);
 		
 		var temp = await rom.readAsBytes();
 		for(int i = 0; i < temp.length; i++){
@@ -25,9 +40,21 @@ class Device {
 	
 	
 	bool print_memory(){
-		for(int i = 0; i < memory.length; i += 10){
-			print(memory[i].toString());
+		
+		String line = "";
+		int count = 0;
+		
+		for(int i = 0; i < memory.length; i++){
+			line += " " + memory[i].toString();
+			count++;
+			
+			if(count == 64){
+				print(line);
+				line = "";
+				count = 0;
+			}
 		}
+		
 		return true;
 	}
 	
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import "device.dart";
 
 Future<void> main() async {
 	var d = Device();
-	await d.load_rom_into_memory();
-	d.print_memory();
+	//await d.load_rom_into_memory();
+	d.print_display_state();
 }
