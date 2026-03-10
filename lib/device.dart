7680e55f8926d4a515dcbde1d4f4aa64fa1f61c3
TheYautja <am.96138669@gmail.com>
2026-03-10 08:46:46 -0300

    add fonts

--- lib/device.dart
+++ lib/device.dart
@@ -14,6 +14,34 @@ class Device {
 	Uint8List registers = Uint8List(16);
 	List<List<bool>> display = List.generate(32, (_) => List.filled(64, false));
 	
+	static const List<int> font_temp = [
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	];
+	
+	Uint8List font = Uint8List.fromList(font_temp);
+	
+	
+	void load_font_into_memory(){
+		for(int i = 0; i < font.length; i++){
+			memory[i] = font[i];
+		}
+	}
+	
 	
 	bool print_display_state(){
 		String line = "";
@@ -46,7 +74,7 @@ class Device {
 		int count = 0;
 		
 		for(int i = 0; i < memory.length; i++){
-			line += " " + memory[i].toString();
+			line += " " + memory[i].toRadixString(16);
 			count++;
 			
 			if(count == 64){
@@ -64,19 +92,43 @@ class Device {
 		
 		int opcode = memory[PC] << 8 | memory[PC + 1];
 		PC += 2;
-		
 		return opcode;
 	}
 	
 	
-	void printopcode(){
-		print(fetch_opcode().toString());
-	}
-	
-	
 	bool decode_opcode(){
+		int opcode = fetch_opcode();
+		switch(opcode){
+
+			
+
+		}
 		return true;
 	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import "device.dart";
 
 Future<void> main() async {
 	var d = Device();
+	d.load_font_into_memory();
 	await d.load_rom_into_memory();
-	d.printopcode();
-	//d.print_memory();
+	d.print_memory();
 	//d.print_display_state();
+	d.decode_opcode();
 }
