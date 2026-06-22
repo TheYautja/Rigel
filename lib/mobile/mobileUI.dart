2b087f30d5a745b2d5e7ef3be2ecc8d7c7c3f47f
TheYautja <am.96138669@gmail.com>
2026-06-22 17:01:17 -0300

    fixed mobile layout

--- lib/mobile/mobileUI.dart
+++ lib/mobile/mobileUI.dart
@@ -12,35 +12,37 @@ class MobileUI extends StatelessWidget{
     @override
     Widget build(BuildContext context){
 
-        double width = MediaQuery.of(context).size.width;
-        double height = MediaQuery.of(context).size.height;
-        double dWidth = width / 1.2;
-        double dHeight = height / 1.2;
-    
-        double pixelWidth = dWidth/64;
-        double pixelHeight = dHeight/32;
 
         return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints){
 
-         return Row(
-            children: [
-                Row(
-                    children: [
-                        Column(children:[InputButton("1", 1), InputButton("2", 2), InputButton("3", 3), InputButton("4", 4),]),
-                        Column(children:[InputButton("A", 5), InputButton("B", 6), InputButton("C", 7), InputButton("D", 8),]),
-                    ],
-                ),
-                GameWidget(game: Rigel(dWidth, dHeight, pixelWidth, pixelHeight)),
-                Row(
-                    children: [
-                        Column(children:[InputButton("E", 9), InputButton("F", 10), InputButton("G", 11), InputButton("H", 12),]),
-                        Column(children:[InputButton("I", 13), InputButton("J", 14), InputButton("K", 15), InputButton("L", 16),]),
-                    ],
-                ),
-            ]
-        );
+            double width = constraints.maxWidth;
+            double height = constraints.maxHeight;
+            double dWidth = width/1.5;
+            double dHeight = height/1.5;
 
+            double pixelWidth = dWidth/64;
+            double pixelHeight = dHeight/32;
 
+            return Row(
+                children: [
+                    Row(
+                        children: [
+                            Column(children:[InputButton("1", 1), InputButton("2", 2), InputButton("3", 3), InputButton("4", 4),]),
+                            Column(children:[InputButton("A", 5), InputButton("B", 6), InputButton("C", 7), InputButton("D", 8),]),
+                        ],
+                    ),
+                    Expanded( child :GameWidget(game: Rigel(dWidth, dHeight, pixelWidth, pixelHeight))),
+                    Row(
+                        children: [
+                            Column(children:[InputButton("E", 9), InputButton("F", 10), InputButton("G", 11), InputButton("H", 12),]),
+                            Column(children:[InputButton("I", 13), InputButton("J", 14), InputButton("K", 15), InputButton("L", 16),]),
+                        ],
+
+                    ),
+
+                ]
+
+            );
 
         });
 
